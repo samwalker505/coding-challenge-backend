804eff4bcc6958511beee5b7bf024094de00c20e
samwalker <samwalker505@gmail.com>
2019-06-07 20:44:28 +0800

    change test script port 8080

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const instance = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "http://localhost:8080"
 });
 test("running test", () => {
   expect(true).toBe(true);
